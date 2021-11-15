8ce407706f01f1d2dc45f672e9c9371d2e1eeb9b
Eric Wang <erwang1151@gmail.com>
2021-11-15 00:22:25 -0500

    Style card status tag and add date to card header

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,19 +1,25 @@
 let domUpdates = {
     displayTrips(userTrips, destinations) {
         userTrips.forEach((trip) => {
+            let statusTagIcon = '<i class="far fa-check-circle"></i>';
+            if(trip.status === "pending") {
+              statusTagIcon = '<i class="fas fa-hourglass-start fa-sm"></i>';
+            } 
+
             tripsGrid.innerHTML += `<article class="card">
             <img src="${destinations.getDestinationByID(trip.destinationID).image}">
             <div class="card-info">
               <div class="card-header">
-                <h3>${destinations.getDestinationByID(trip.destinationID).destination}</h3>
-                <article class="status-tag">${trip.status}</article>
+                <h3><b>${destinations.getDestinationByID(trip.destinationID).destination}</b>&nbsp;&nbsp; ${trip.date}</h3>
+                <article class="status-tag ${trip.status}">${statusTagIcon}${trip.status}</article>
               </div>
               <div class="card-info-tags">
                 <article class="card-tag"><p id="travelersTag">${trip.travelers} travelers</p></article>
                 <article class="card-tag"><p id="durationTag">${trip.duration} days</p></article>
               </div>
             </div>
-          </article>`
+          </article>`;
+
         });
     },
 
